a71ea16c953c40b203a98c99a3e9db20d2126780
vigneshkg <kgvignesh07@gmail.com>
2019-08-05 04:02:12 +0530

    qed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  submitData(event) {
+    window.alert("Welcome " + event + " !");
+  }
   constructor() { }
 
   ngOnInit() {
